632a4e0a8d075705bbf9a5c5a7c770ca4ec8bd5e
IagoFreitas12 <88669758+IagoFreitas12@users.noreply.github.com>
2022-04-08 13:17:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var nome = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var file = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin"); 
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+fields.forEach(function(field, index){
+    if (field.name == "gender"){
+    
+        if(field.checked){
+            console.log("Sim", field)
+        }      
+
+    } else {
+
+
+    }
+    
+});
